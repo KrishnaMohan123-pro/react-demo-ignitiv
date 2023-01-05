a4b50ae629d6676593a744969e141d7a20077663
Krishna Mohan <krishna.m@ignitiv.com>
2023-01-05 17:22:13 +0530

    pages initial commit

--- src/pages/Questions/index.jsx
+++ src/pages/Questions/index.jsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function Questions() {
     return <h1>Questions</h1>
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function User() {
     return <h1>User</h1>
 }
