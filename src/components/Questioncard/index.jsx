76e7f91b01f06cf75ddfdca60a9e9c1c9ff5838a
DESKTOP-76CI5SG\Ignitiv <sp4996932@gmail.com>
2023-01-06 16:03:32 +0530

    Question Card Created

---
+++ src/components/Questioncard/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+// import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+ import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+// import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormControl from '@material-ui/core/FormControl';
+// import FormLabel from '@material-ui/core/FormLabel';
+import './index.css';
+import AnswerCard from '../AnswerCard';
+
+function QuestionCard(props) {
+      console.log(props.data)
+    const Data = props.data;
+    // const classes = useStyles();
+    // const bull = <span className={classes.bullet}>•</span>
+        return (
+            <Grid container spacing={3}>
+                    {Data.map((item, index)=> (
+                        <Grid item xs={12} key={item.id}>
+                          {console.log("item", item)}
+                            
+                            <Card className="QuestionCard Container" variant="outlined">
+                              <CardContent>
+                                <Typography variant="h5" component="h2">
+                                <span>{index+1}.</span><span> {item.question}</span>
+                                </Typography>
+                              </CardContent>
+                              <CardActions>
+                                  {/*
+                                      <FormControl component="fieldset">
+                                        <FormLabel component="legend">Gender</FormLabel>
+                                        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                                          <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                          <FormControlLabel value="other" control={<Radio />} label="Other" />
+                                          <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
+                                        </RadioGroup>
+                                      </FormControl> */}
+                                  <AnswerCard item={item}/>
+                              </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+            </Grid>
+          )
+    }
+
+  export default QuestionCard;
+
