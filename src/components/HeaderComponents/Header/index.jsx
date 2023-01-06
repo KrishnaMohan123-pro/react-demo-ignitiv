7ea8d0215242da547180da9d5248dc807ac92f07
Uddeshya_Kumar <uddeshya.k@ignitiv.com>
2023-01-06 17:53:17 +0530

    Uddeshya-Header

---
+++ src/components/HeaderComponents/Header/index.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import logo from "../../../resources/light-logo-final.png";
+
+export default function Header() {
+    return <>
+        <div className="headerMain">
+            <Grid container>
+                <Grid item xs={4}>
+                    <Link to="/">
+                        <img className="ignitivLogo" src={logo} alt="Ignitiv Logo"></img>
+                    </Link>
+                </Grid>
+            </Grid>
+        </div>
+    </>
+}
