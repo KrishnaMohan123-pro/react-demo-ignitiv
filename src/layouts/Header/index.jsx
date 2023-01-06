97eb3e8530927a274c17e3fc2bff420612b971fd
rkDas2949 <ranjeet.d@ignitiv.com>
2023-01-06 14:43:47 +0530

    Home page design updated

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
-    return <div className="header">
-        <div className="top-header">
-        </div>
-        <div className="nav-bar"></div>
+return (
+<div className="header">
+    <div className="top-header">
+        <h1>Header part</h1>
     </div>
+    <div className="nav-bar"></div>
+</div>
+);
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,14 @@
+import { Container } from '@material-ui/core';
+import './index.css';
 export default function Home() {
-    return <h1>Home</h1>
+return (
+<Container>
+    <div class="main">
+        <h1>Quiz API URL Generator</h1>
+        <p>Quiz API is a simple HTTP REST API for technical quizzes including a wide variety of topics like:</p>
+        <p>Test your knowledge or easily embed a quiz on your website with the Quiz API</p>
+    </div>
+</Container>
+
+);
 }
