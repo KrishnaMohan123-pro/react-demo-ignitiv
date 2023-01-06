900512dfe2beb563b867be5ed5d6932a39980848
rkDas2949 <ranjeet.d@ignitiv.com>
2023-01-06 16:37:35 +0530

    revised file home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,13 @@ import './index.css';
 export default function Home() {
 return (
     <div class="main">
-        <h1>Quiz API URL Generator</h1>
+        <h1>Ignitiv Interview Test</h1>
+        <h4>About Ignitiv</h4>
+        <p></p>
+        <h4>We are hiring for:</h4>
+        <ul>
+            <li></li>
+        </ul>
         <p>Quiz API is a simple HTTP REST API for technical quizzes including a wide variety of topics like:</p>
         <p>Test your knowledge or easily embed a quiz on your website with the Quiz API</p>
     </div>
