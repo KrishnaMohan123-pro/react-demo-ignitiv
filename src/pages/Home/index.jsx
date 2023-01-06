5b98c00a497e6ef7f4fb7097f3aefc5d758fb707
rkDas2949 <ranjeet.d@ignitiv.com>
2023-01-06 17:27:08 +0530

    Home page revised

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,13 +4,21 @@ export default function Home() {
 return (
     <div class="main">
         <h1>Ignitiv Interview Test</h1>
-        <h4>About Ignitiv</h4>
-        <p></p>
+        <h2>About Ignitiv</h2>
+        <p>Ignitiv is a products and consulting MNC with development centers in the US, Canada and India. We deliver solutions that help global brands harness emerging disruptive technologies to enrich their customers’ experience in ways not imagined before.</p>
+        <p>At Ignitiv, we are always pushing our boundaries. In doing so, we discover new capabilities and develop a distinct point of view about the challenges and opportunities affecting our customers. Towards that goal, we are always looking for people to join our team who are independent thinkers and have the drive to understand and solve business problems of our clients in unique and dramatically better ways. 
+            We are currently geared up for exceptional growth and onboarding leaders in our team. If you believe in your technical and learning capabilities, we may have a suitable opportunity that will help you uncover the secret path of expedited career growth!</p>
         <h4>We are hiring for:</h4>
-        <ul>
-            <li></li>
-        </ul>
-        <p>Quiz API is a simple HTTP REST API for technical quizzes including a wide variety of topics like:</p>
+    <ul>
+        <li><a href="https://www.ignitiv.com/jobs/xstore-developer/">Xstore Developer</a></li>
+        <li><a href="https://www.ignitiv.com/jobs/qa-analyst/">QA Analyst</a></li>
+        <li><a href="https://www.ignitiv.com/jobs/software-developer-front-end-2/">Software Developer (Front-End)</a></li>
+        <li><a href="https://www.ignitiv.com/jobs/senior-developer-front-end/">Senior Developer (Front-End)</a></li>
+        <li><a href="https://www.ignitiv.com/jobs/software-developer-front-end/">Sr. Developer / Tech Lead – eComm – Full Stack</a></li>
+        <li><a href="https://www.ignitiv.com/jobs/back-end-sr-developer-analytics-2/">Back-End Sr. Developer (Analytics)</a></li>
+        <li><a href="https://www.ignitiv.com/jobs/sr-developer-back-end-tech-lead/">Sr. Developer (Back-End) / Tech Lead</a></li>
+    </ul>
+        <p>Ignitiv provide a mock interview platform for fresher/experinced technical questions including a wide variety of topics like:</p>
         <p>Test your knowledge or easily embed a quiz on your website with the Quiz API</p>
     </div>
 );
