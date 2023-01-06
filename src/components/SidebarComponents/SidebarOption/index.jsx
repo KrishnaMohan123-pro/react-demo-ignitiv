0619469bbf5b2231ff09638f2de946a30863401c
Krishna Mohan <krishna.m@ignitiv.com>
2023-01-06 16:41:57 +0530

    add css for sidebar questions

---
+++ src/components/SidebarComponents/SidebarOption/index.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import { FormControlLabel, Radio } from "@material-ui/core";
+
+
+
+export default function SidebarOptions(props) {
+    console.log('SidebarOptions props', props);
+    return  <FormControlLabel
+    value={props.value}
+    control={<Radio className='radio-button' value={props.value} color='primary'/>}
+    label={props.value}
+    labelPlacement="end"
+    className="side-bar-option"
+  />
+}
--- src/components/SidebarComponents/SidebarQuestions/index.jsx
+++ src/components/SidebarComponents/SidebarQuestions/index.jsx
@@ -1,5 +1,34 @@
+import { Typography } from '@material-ui/core';
+import { useState } from 'react';
+import SidebarOption from '../SidebarOption';
+
+import './index.css'
+
+const quizApiParams = require('../../../utility/quizApiParams.json');
 export default function SidebarQuestions() {
+    const [showMore, setShowMore] = useState({options1: false, options2: false});
+    const { difficulty } = quizApiParams;
+    const numberOfQuestion = [];
+    for(let i = 10; i < 40; i+=5){
+        numberOfQuestion.push(i);
+    }
+    console.log(difficulty) 
     return <div className="side-bar-contents">
-        Sidebar Questions
+        <div className='options'>
+        <Typography variant='h6'>Difficulty</Typography>
+        {difficulty.map((item, index) => <SidebarOption key={index} value={item} />)}
+        </div>
+        <div className='options'>
+        <Typography variant='h6'>Number of Questions</Typography>
+        {numberOfQuestion.map((item, index) => {
+            if(showMore.options2) {
+                return <SidebarOption key={index} value={item} />
+            } else if (index < 5) {
+                return <SidebarOption key={index} value={item} />
+            } else {
+                return null;
+            }
+        })}
+        </div>
     </div>
 }
