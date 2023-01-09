2484426adad560ed164697b143b73c6bfba90d79
Krishna Mohan <krishna.m@ignitiv.com>
2023-01-09 11:39:36 +0530

    add category check for questions body

--- src/pages/Questions/index.jsx
+++ src/pages/Questions/index.jsx
@@ -12,17 +12,23 @@ export default function Questions() {
     const category = searchParams.get('category')
     const [questionsData, setQuestionsData] = useState({isLoading: true});
     useEffect(()=>{
-        fetchApi(createQuery(category))
-        .then(res =>res.json())
-        .then(data => {
-            setQuestionsData({questions:data, isLoading: false});
-        });
+         if (category) {
+            fetchApi(createQuery(category))
+            .then(res =>res.json())
+            .then(data => {
+                setQuestionsData({questions:data, isLoading: false});
+            });
+        }
     }, [category])
     return (<div>
-        {questionsData.isLoading ? <div>Loading</div>: <div>
-            <h1>Questions</h1>
-            <QuestionCard data={questionsData.questions} />
-            </div>}
+        {category ? 
+            questionsData.isLoading ? 
+                <div>Loading</div> : 
+                <div>
+                  <h1>Questions</h1>
+                  <QuestionCard data={questionsData.questions} />
+                </div> : 
+            <div>Please check the query</div>}
         </div>
     )
 }
