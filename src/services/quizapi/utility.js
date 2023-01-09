8a57087e41b2a453611a565a523cb43c950fe4ac
Krishna Mohan <krishna.m@ignitiv.com>
2023-01-09 11:13:43 +0530

    add default values when create query for fetch

--- src/services/quizapi/utility.js
+++ src/services/quizapi/utility.js
@@ -1,3 +1,3 @@
-export default function createQuery(category, difficulty, limit, tags) {
+export default function createQuery(category = '', difficulty = '', limit = '', tags = '') {
     return `&category=${category}&limit=${limit}&difficulty=${difficulty}&tags=${tags}`;
 }
