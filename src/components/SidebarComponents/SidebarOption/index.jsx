b7d527a3cd1ca4f815f7effdbb3f8739e8098521
Krishna Mohan <krishna.m@ignitiv.com>
2023-01-09 14:07:30 +0530

    add event listener for sidebar option selection

--- src/components/SidebarComponents/SidebarOption/index.jsx
+++ src/components/SidebarComponents/SidebarOption/index.jsx
@@ -1,15 +1,36 @@
 import './index.css'
 import { FormControlLabel, Radio } from "@material-ui/core";
+import { useSearchParams } from 'react-router-dom';
 
 
 
 export default function SidebarOptions(props) {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  function handleOptionSelect(e) {
+    e.preventDefault();
+    let query;
+    const value = e.currentTarget.innerText;
+    const category = searchParams.get('category');
+    const limit = searchParams.get('limit') ? searchParams.get('limit') : '10';
+    const difficulty = searchParams.get('difficulty') ? searchParams.get('difficulty') : 'easy';
+    if (props.type === 'difficulty') {
+      query = `category=${category}&difficulty=${value}&limit=${limit}`
+    } else {
+      query = `category=${category}&difficulty=${difficulty}&limit=${value}`
+    }
+    
+    setSearchParams(query);
+  }
+
     return  <FormControlLabel
     value={props.value}
     control={<Radio className='radio-button' value={props.value} color='primary'/>}
     label={props.value}
     labelPlacement="end"
     className="side-bar-option"
-    checked={false}
+    checked={props.selected}
+    onClick={handleOptionSelect}
   />
 }
