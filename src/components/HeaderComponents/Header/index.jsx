58710566ba1d25ef00e9dc0e19f80bb7843c320e
Uddeshya_Kumar <uddeshya.k@ignitiv.com>
2023-01-10 18:25:49 +0530

    uddeshya - SignIn form

--- src/components/HeaderComponents/Header/index.jsx
+++ src/components/HeaderComponents/Header/index.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./index.css";
 import Grid from '@material-ui/core/Grid';
 import logo from "../../../resources/light-logo-final.png";
+import Modal from "../../Modal"
 
 export default function Header() {
     return <>
@@ -11,6 +12,10 @@ export default function Header() {
                     <Link to="/">
                         <img className="ignitivLogo" src={logo} alt="Ignitiv Logo"></img>
                     </Link>
+                </Grid>
+                <Grid className="blankSpace" item xs={6}></Grid>
+                <Grid className="loginLink" item xs={2}>
+                    <Modal buttonText={`Sign In / Sign Up`} />
                 </Grid>
             </Grid>
         </div>
