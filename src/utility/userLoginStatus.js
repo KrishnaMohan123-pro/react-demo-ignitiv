d85595af4b7d67ec8f96f546eb947adcba51feda
Krishna Mohan <krishna.m@ignitiv.com>
2023-01-12 13:07:51 +0530

    update userLoginStatus condition

--- src/utility/userLoginStatus.js
+++ src/utility/userLoginStatus.js
@@ -2,4 +2,5 @@ const cookie = document.cookie;
 const cookieArr = cookie.split('; ');
 const userCookie = cookieArr.find(e => e.includes('isUserLoggedIn'));
 const isUserLoggedIn = userCookie ? userCookie.split('=')[1]: 'false';
-export default isUserLoggedIn === 'true';
+isUserLoggedIn.includes('true');
+export default isUserLoggedIn.includes('true');
